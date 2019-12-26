7ddd74aef60fe2df7272aa7f739e01a9c3b53545
Dmytro <dimalg400@gmail.com>
2019-12-26 17:06:23 +0200

    add postgreSQL

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.postgres',
 
 ]
 
@@ -77,8 +78,15 @@ WSGI_APPLICATION = 'bookmarks.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'bookmarks_db',
+        'USER': 'postgres',
+        'PASSWORD': '13111999',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+
     }
 }
 
